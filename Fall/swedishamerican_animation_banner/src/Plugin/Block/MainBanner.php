8fd7918183d9fc1b84b72583cf6f828ecfe9251b
Tim Grable <tgrable@trekk.com>
2017-11-22 10:57:34 -0600

    initial commit

---
+++ Fall/swedishamerican_animation_banner/src/Plugin/Block/MainBanner.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\swedishamerican_animation_banner\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'SwedishAmerican Animation Banner'
+ *
+ * @Block(
+ *   id = "swedishamerican_animation_banner",
+ *   admin_label = @Translation("SwedishAmerican Animation Banner"),
+ * )
+ */
+class MainBanner extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // Add '#cache' => ['max-age' => 0], in for testing
+    return [
+      '#theme' => 'main_banner',
+      '#nodes' => null,
+      '#attached' => array(
+        'library' => array('swedishamerican_animation_banner/swedishamerican-animation-banner'),
+        ),
+    ];
+  }
+}
